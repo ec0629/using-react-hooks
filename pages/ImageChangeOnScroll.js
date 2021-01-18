2f45c8e9447d976fe9d7c39053e7d2bbbf1c4771
Jeff Simonitto <jsimonitto@gmail.com>
2021-01-18 11:53:33 -0500

    changes to prep for example about useEffect dependency list

--- pages/ImageChangeOnScroll.js
+++ pages/ImageChangeOnScroll.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageToggleOnScroll from "../src/ImageToggleOnScroll";
 
 const imageNames = [
@@ -15,11 +15,28 @@ const imageNames = [
 ];
 
 const ImageChangeOnScroll = () => {
+  const [currentImageId, setCurrentImageId] = useState(0);
+  const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+  useEffect(() => {
+    window.document.title = `ImageId: ${currentImageId}`;
+    console.log(`useEffect: setting title to ${currentImageId}`);
+  }); // we have not supplied an empty array or an array of
+  // dependencies and therefore it will run on any change in state
+
   return (
     <div>
+      <span>Mouse Event Count: {mouseEventCnt}</span>
       {imageNames.map((imageId) => {
         return (
-          <div key={imageId}>
+          <div
+            key={imageId}
+            onMouseOver={() => {
+              setCurrentImageId(imageId);
+              setMouseEventCnt(mouseEventCnt + 1);
+              console.log(`onMouseOver: ${imageId}`);
+            }}
+          >
             <ImageToggleOnScroll
               primaryImg={`/static/img${imageId}-thmb-bw.jpg`}
               secondaryImg={`/static/img${imageId}-thmb.jpg`}
