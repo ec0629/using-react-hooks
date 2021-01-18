f8b362255424ab1c32ce66a3e714382fc7601deb
Jeff Simonitto <jsimonitto@gmail.com>
2021-01-18 08:24:41 -0500

    example of useRef hook

--- src/ImageToggleOnMouseOver.js
+++ src/ImageToggleOnMouseOver.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const ImageToggleOnMouseOver = (props) => {
-  const { primaryImg, secordaryImg, ...rest } = props;
-  return <img src={primaryImg} {...rest} />;
+  const { primaryImg, secondaryImg, ...rest } = props;
+  const imageRef = useRef(null);
+  return (
+    <img
+      onMouseOver={() => {
+        console.log("Mouse over");
+        imageRef.current.src = secondaryImg;
+      }}
+      onMouseOut={() => {
+        imageRef.current.src = primaryImg;
+      }}
+      src={primaryImg}
+      ref={imageRef}
+      {...rest}
+    />
+  );
 };
 
 export default ImageToggleOnMouseOver;
